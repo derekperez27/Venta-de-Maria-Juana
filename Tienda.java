68b4da96c211c613500276545db33acbc001f445
derekperez27 <derekpp2016@gmail.com>
2025-02-20 16:50:37 +0100

    Add files via upload

---
+++ Tienda.java
@@ -0,0 +1,39 @@
+package tienda_hierbas;
+
+public class Tienda {
+    public Producto[] productos;
+
+    public Tienda() {
+        productos = new Producto[4];
+        productos[0] = new Producto("Maria Juana", "Mexico", 1.70);
+        productos[1] = new Producto("Hierba de los bosques", "Andorra", 4.20);
+        productos[2] = new Producto("La seta feliz", "Marruecos", 2.47);
+        productos[3] = new Producto("El oro verde", "Palestina", 1.33);
+    }
+
+    public Producto[] buscarPorNombre(String nombreParametro) {
+        Producto[] resultado = new Producto[productos.length];
+        int contador = 0;
+        for (int i = 0; i < productos.length; i++) {
+            if (productos[i].nombre.contains(nombreParametro)) {
+                resultado[contador] = productos[i];
+                contador++;
+            }
+        }
+        return resultado;
+    }
+
+    public Producto[] buscarPorOrigen(String origenParametro) {
+        Producto[] resultado = new Producto[productos.length];
+        int contador = 0;
+        for (int i = 0; i < productos.length; i++) {
+            if (productos[i].origen.equals(origenParametro)) {
+                resultado[contador] = productos[i];
+                contador++;
+            }
+        }
+        return resultado;
+    }
+}
+
+
